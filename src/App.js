8d320cbc5293be4e6cf685adef9ad71b9242cb8f
Shaggy <hamxa1331u@gmail.com>
2021-06-13 00:11:59 +0500

    bare working

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Services from "./pages/Services";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Portfolio from "./pages/Portfolio";
+import Blogs from "./pages/BlogPage";
+import ContactUs from "./pages/ContactUs";
+import BlogDetails from "./pages/BlogDetailsPage";
+import GetInTouch from "./components/contact/GetInTouch";
+import Footer from "./components/footer/Footer";
+function App() {
+  return (
+    <>
+      <Router>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={AboutUs} exact />
+        <Route path="/services" component={Services} exact />
+        <Route path="/blogs" component={Blogs} exact />
+        <Route path="/contact" component={ContactUs} exact />
+        <Route path="/Portfolio" component={Portfolio} exact />
+        <Route path="/blogs/:id" component={BlogDetails} exact />
+        <GetInTouch />
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
